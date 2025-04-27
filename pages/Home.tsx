1140c207f10b11647428c23e6dc0b6049fb371c2
santiago aranda <santib05@outlook.com>
2025-04-26 21:46:53 -0300

    segundo commit, distintas paginas

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/componentes/Navbar';
 function Home() {
   return (
     <>
-      
+      <Navbar />
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold mb-4">Bienvenido a Mi Sistema</h1>
         <p className="text-lg text-gray-600">Gestiona turnos, productos y más.</p>
